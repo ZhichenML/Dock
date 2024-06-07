afc283ce12da1b064cef38bbbffbd2a8569adb20
ZhichenML <v123gz@163.com>
2024-06-07 14:34:51 +0800

    gen3d in obabel

--- docking/docking_clean.sh
+++ docking/docking_clean.sh
@@ -19,52 +19,48 @@ size_z=$9
 remove_dir=${10:-true}  # Default value is true if not provided
 
 # Get the base names of the input files
-protein_base=$(basename $protein_file)
-ligand_base=$(basename $ligand_file)
+protein_base=$(basename "$protein_file")
+ligand_base=$(basename "$ligand_file")
 
 # Create a temporary directory for intermediate files
 tmp_dir="./tmp_${protein_base%.*}" 
-mkdir -p $tmp_dir
+mkdir -p "$tmp_dir"
 
-# Check and transform ligand file from .mol to .pdb if necessary
-ligand_extension="${ligand_file##*.}"
-if [ "$ligand_extension" = "mol" ]; then
-  ligand_pdb="${ligand_base%.*}.pdb"
-  obabel $ligand_file -O $tmp_dir/$ligand_pdb
-  if [ $? -ne 0 ]; then
-    echo "Transformation of ligand file from .mol to .pdb failed"
-    [[ "$remove_dir" == "true" ]] && rm -r $tmp_dir
-    exit 1
-  fi
-  ligand_file=$tmp_dir/$ligand_pdb
-else
-  cp $ligand_file $tmp_dir/
-  ligand_file=$tmp_dir/$ligand_base
+# Convert ligand file to .pdbqt format using convert_ligand_to_pdbqt.sh
+./convert_ligand_to_pdbqt.sh "$ligand_file" "$tmp_dir/${ligand_base%.*}.pdbqt"
+if [ $? -ne 0 ]; then
+  echo "Conversion of ligand file to .pdbqt failed"
+  [[ "$remove_dir" == "true" ]] && rm -r "$tmp_dir"
+  exit 1
 fi
+ligand_pdbqt="$tmp_dir/${ligand_base%.*}.pdbqt"
 
-# Convert protein and ligand files to PDBQT format
-obabel $protein_file -xr -O $tmp_dir/${protein_base%.*}.pdbqt
-obabel $ligand_file -O $tmp_dir/${ligand_base%.*}.pdbqt -h
+# Convert protein file to PDBQT format
+obabel "$protein_file" -xr -O "$tmp_dir/${protein_base%.*}.pdbqt"
+if [ $? -ne 0 ]; then
+  echo "Conversion of protein file to .pdbqt failed"
+  [[ "$remove_dir" == "true" ]] && rm -r "$tmp_dir"
+  exit 1
+fi
+protein_pdbqt="$tmp_dir/${protein_base%.*}.pdbqt"
 
-# Define output filenames based on input filenames
-protein_pdbqt=$tmp_dir/${protein_base%.*}.pdbqt
-ligand_pdbqt=$tmp_dir/${ligand_base%.*}.pdbqt
-output_pdbqt=$tmp_dir/${ligand_base%.*}-redock.pdbqt
+# Define output filename based on input filenames
+output_pdbqt="$tmp_dir/${ligand_base%.*}-redock.pdbqt"
 
 # Run smina with the specified parameters
-./smina.static -r $protein_pdbqt -l $ligand_pdbqt --center_x $center_x --center_y $center_y --center_z $center_z --size_x $size_x --size_y $size_y --size_z $size_z --cpu 64 --exhaustiveness 16 -o $output_pdbqt
+./smina.static -r "$protein_pdbqt" -l "$ligand_pdbqt" --center_x "$center_x" --center_y "$center_y" --center_z "$center_z" --size_x "$size_x" --size_y "$size_y" --size_z "$size_z" --cpu 64 --exhaustiveness 16 -o "$output_pdbqt"
 
 # Confirm the docking run
 if [ $? -eq 0 ]; then
   echo "Docking run successful: $output_pdbqt"
 else
   echo "Docking run failed"
-  [[ "$remove_dir" == "true" ]] && rm -r $tmp_dir
+  [[ "$remove_dir" == "true" ]] && rm -r "$tmp_dir"
   exit 1
 fi
 
 # Extract minimizedAffinity from the output PDBQT file
-minimized_affinity=$(grep -m 1 'REMARK minimizedAffinity' $output_pdbqt | awk '{print $3}')
+minimized_affinity=$(grep -m 1 'REMARK minimizedAffinity' "$output_pdbqt" | awk '{print $3}')
 
 # Output the minimized affinity
 if [ -n "$minimized_affinity" ]; then
@@ -75,5 +71,5 @@ fi
 
 # Clean up the temporary directory based on the remove_dir parameter
 if [[ "$remove_dir" == "true" ]]; then
-  rm -r $tmp_dir
+  rm -r "$tmp_dir"
 fi
